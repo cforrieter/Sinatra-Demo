29161d14e93b82c5863f8730a1bf63241e8afd0c
cforrieter <cforrieter@gmail.com>
2015-09-21 03:28:30 +0000

    Add url to messages. If not prefixed with http, add that to url before saving to database

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,11 @@
 class Message < ActiveRecord::Base
-  validates :title, :author, :content, presence: true
+  validates :author, :content, presence: true
+
+  before_save :validate_url, if: :url
+
+  def validate_url
+    unless /https?:\/\//.match(self.url)
+      self.url = "http://#{url}"
+    end
+  end
 end
